4af50330ed98915801ca70e431995e6f5eb8eccc
Igal Kreichman <igal@skycure.com>
2014-08-23 01:38:40 +0300

    Minor fix

--- AndroidApp/app/src/main/java/com/zayandroid/fineandall/mainapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/zayandroid/fineandall/mainapp/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends Activity  implements ExploreQuestionsFragment.
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        menu.add("blah").setVisible(true);
         return super.onCreateOptionsMenu(menu);
     }
 
