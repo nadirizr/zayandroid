bf4331e4a0805fab800cced28e5b77f59b9c4d1d
Igal Kreichman <igal@skycure.com>
2014-08-23 13:56:25 +0300

    Update QuestionResultsFragment.java

--- AndroidApp/app/src/main/java/com/zayandroid/fineandall/mainapp/QuestionResultsFragment.java
+++ AndroidApp/app/src/main/java/com/zayandroid/fineandall/mainapp/QuestionResultsFragment.java
@@ -165,8 +165,6 @@ public class QuestionResultsFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-
-
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
