853b4b7477bc8e4c0d0f61de3ae2c8a8476f2f4a
Nadir Izrael <nadir.izr@gmail.com>
2014-08-23 10:44:29 +0300

    Fixed some typos and created config for database.

--- server/lib/questions.js
+++ server/lib/questions.js
@@ -12,13 +12,13 @@ exports.addStubQuestions = function() {
     imageUrl: 'http://upload.wikimedia.org/wikipedia/en/4/43/The_Ramen_Girl_poster.jpg',
     yes_count: 13,
     no_count: 4
-  };
+  });
   var q2 = new Question({
     text: 'יש לה 2 קרציות בין הבהונות',
     imageUrl: 'http://upload.wikimedia.org/wikipedia/en/4/43/The_Ramen_Girl_poster.jpg',
     yes_count: 20,
     no_count: 1
-  };
+  });
 
   q1.save();
   q2.save();
