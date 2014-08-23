47bd6468da9915201b3e068b0e8021f5f1ebee28
Nadir Izrael <nadir.izr@gmail.com>
2014-08-23 11:56:15 +0300

    Added MongoDB from MongoHQ to config.

--- server/lib/questions.js
+++ server/lib/questions.js
@@ -8,22 +8,22 @@ exports.getAllQuestions = function(callback) {
 
 exports.addStubQuestions = function() {
   // Remove existing questions.
-  Question.find({}).remove().exec();
+  Question.find({}).remove(function(err, res) {
+    // Add new stub questions.
+    var q1 = new Question({
+      text: 'יש לה קרציה בין הבהונות',
+      imageUrl: 'http://upload.wikimedia.org/wikipedia/en/4/43/The_Ramen_Girl_poster.jpg',
+      yes_count: 13,
+      no_count: 4
+    });
+    var q2 = new Question({
+      text: 'יש לה 2 קרציות בין הבהונות',
+      imageUrl: 'http://upload.wikimedia.org/wikipedia/en/4/43/The_Ramen_Girl_poster.jpg',
+      yes_count: 20,
+      no_count: 1
+    });
 
-  // Add new stub questions.
-  var q1 = new Question({
-    text: 'יש לה קרציה בין הבהונות',
-    imageUrl: 'http://upload.wikimedia.org/wikipedia/en/4/43/The_Ramen_Girl_poster.jpg',
-    yes_count: 13,
-    no_count: 4
+    q1.save();
+    q2.save();
   });
-  var q2 = new Question({
-    text: 'יש לה 2 קרציות בין הבהונות',
-    imageUrl: 'http://upload.wikimedia.org/wikipedia/en/4/43/The_Ramen_Girl_poster.jpg',
-    yes_count: 20,
-    no_count: 1
-  });
-
-  q1.save();
-  q2.save();
 };
